a2894f43f7e9982472242558b9dde789e1775011
gr402_sadev <d.salato@mail.ru>
2023-02-02 13:14:23 +0700

    start

---
+++ js/main.js
@@ -0,0 +1,15 @@
+Vue.component('product', {
+    template: `
+        <div class="product">
+            <h1>Работает</h1>
+       </div>
+       
+    `,
+})
+let app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+
+})
