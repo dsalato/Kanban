b87e5388aa5e094103a5c230cd5f045c3a913843
gr402_sadev <d.salato@mail.ru>
2023-02-03 11:40:36 +0700

    update card done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let eventBus = new Vue()
 Vue.component('Cards', {
     template: `
        <div class="Cards">
-           <h1>Заметки</h1>
+           <h1>Kanban доска</h1>
                <create_card></create_card>
                <div class="cards_inner">
                     <div class="cards_item">
@@ -38,6 +38,7 @@ Vue.component('Cards', {
         eventBus.$on('addColumnFirst', card => {
                 this.columnFirst.push(card)
 
+
         })
         eventBus.$on('addColumnSecond', card => {
                 this.columnSecond.push(card)
@@ -50,8 +51,6 @@ Vue.component('Cards', {
             this.columnSecond.splice(this.columnSecond.indexOf(card), 1)
 
 
-
-
         })
         eventBus.$on('addColumnFourth', card => {
             this.columnFourth.push(card)
@@ -60,11 +59,7 @@ Vue.component('Cards', {
         })
 
     },
-    methods: {
-        removeTodo(id){
-            console.log(id)
-        }
-    }
+
 })
 Vue.component('Columns1', {
     template: `
@@ -73,12 +68,26 @@ Vue.component('Columns1', {
                     <span>
                         <p>Описание: {{column.text}}</p>
                         <p>Срок дэдлайна: {{column.deadline}}</p>
-                        <p>Дата создания: {{column.data}}</p>
+                        <p v-show="!column.edit">Дата создания: {{column.data}}</p>
+                        <p v-show="column.cancel">Дата изменения: {{column.dataEdit}}</p>
                         
                         <input type="checkbox"
                         @change.prevent="updateColumn(column)">
                     </span>
                     <button class="column_btn" @click="deleteCard(column)" >&times</button>
+                    <a href="#" v-show="!column.edit" v-on:click="column.edit = true">Изменить</a>
+                    <a href="#" v-show="column.edit" 
+                        v-on:click="column.edit = false" 
+                        v-on:click="column.cancel = true" 
+                        v-on:click.prevent="dateEditCard(column)">Закрыть</a>
+                        <form v-show="column.edit">
+                            <label for="name1">Изменить задачу:</label>
+                            <input class="form_input_card" id="name1" v-model="column.name" placeholder="task" >
+                            <label for="name2">Изменить описание:</label>
+                            <textarea class="form_input_card" id="name2" v-model="column.text"></textarea>
+                            
+                        </form>
+
                 </div>
        </div>`,
     props: {
@@ -91,11 +100,20 @@ Vue.component('Columns1', {
 
     },
     methods: {
+        updateColumn(card) {
+                eventBus.$emit('addColumnSecond', card)
+
+        },
         deleteCard(card) {
             console.log(this.columnFirst)
             this.columnFirst.splice(this.columnFirst.indexOf(card), 1)
             console.log(this.columnFirst)
         },
+        dateEditCard(card) {
+            card.dataEdit = new Date().toLocaleString()
+        }
+
+
     },
 
 })
@@ -106,11 +124,22 @@ Vue.component('Columns2', {
                     <span>
                         <p>Описание: {{column.text}}</p>
                         <p>Срок дэдлайна: {{column.deadline}}</p>
-                        <p>Дата создания: {{column.data}}</p>
+                        <p v-show="!column.edit">Дата создания: {{column.data}}</p>
+                        <p v-show="column.cancel">Дата изменения: {{column.dataEdit}}</p>
                         
                         <input type="checkbox"
                         @change.prevent="updateColumnTwo(column)">
-                        
+                        <a href="#" v-show="!column.edit" v-on:click="column.edit = true">Изменить</a>
+                        <a href="#" v-show="column.edit" 
+                            v-on:click="column.edit = false" 
+                            v-on:click="column.cancel = true" 
+                            v-on:click.prevent="dateEditCard(column)">Закрыть</a>
+                            <form v-show="column.edit">
+                                <label for="name1">Изменить задачу:</label>
+                                <input class="form_input_card" id="name1" v-model="column.name" placeholder="task" >
+                                <label for="name2">Изменить описание:</label>
+                                <textarea class="form_input_card" id="name2" v-model="column.text"></textarea>
+                            </form>
                     </span>
                 </div>
        </div>`,
@@ -127,6 +156,9 @@ Vue.component('Columns2', {
             eventBus.$emit('addColumnThird', card)
 
         },
+        dateEditCard(card) {
+            card.dataEdit = new Date().toLocaleString()
+        }
     }
 
 })
@@ -137,11 +169,23 @@ Vue.component('Columns3', {
                     <span>
                         <p>Описание: {{column.text}}</p>
                         <p>Срок дэдлайна: {{column.deadline}}</p>
-                        <p>Дата создания: {{column.data}}</p>
+                        <p v-show="!column.edit">Дата создания: {{column.data}}</p>
+                        <p v-show="column.cancel">Дата изменения: {{column.dataEdit}}</p>
                         
                         <input type="checkbox"
                         @change.prevent="updateColumnThird(column)">
-                        
+                        <a href="#" v-show="!column.edit" v-on:click="column.edit = true">Изменить</a>
+                        <a href="#" v-show="column.edit" 
+                            v-on:click="column.edit = false" 
+                            v-on:click="column.cancel = true" 
+                            v-on:click.prevent="dateEditCard(column)">Закрыть</a>
+                            <form v-show="column.edit">
+                                <label for="name1">Изменить задачу:</label>
+                                <input class="form_input_card" id="name1" v-model="column.name" placeholder="task" >
+                                <label for="name2">Изменить описание:</label>
+                                <textarea class="form_input_card" id="name2" v-model="column.text"></textarea>
+                            
+                        </form>
                     </span>
                 </div>
        </div>`,
@@ -158,6 +202,9 @@ Vue.component('Columns3', {
             eventBus.$emit('addColumnFourth', card)
 
         },
+        dateEditCard(card) {
+            card.dataEdit = new Date().toLocaleString()
+        }
     }
 
 })
@@ -168,7 +215,8 @@ Vue.component('Columns4', {
                     <span>
                         <p>Описание: {{column.text}}</p>
                         <p>Срок дэдлайна: {{column.deadline}}</p>
-                        <p>Дата создания: {{column.data}}</p>
+                        <p v-show="!column.edit">Дата создания: {{column.data}}</p>
+                        <p v-show="column.cancel">Дата изменения: {{column.dataEdit}}</p>
                         
                         
                     </span>
@@ -186,24 +234,25 @@ Vue.component('Columns4', {
 
 
 })
+
 Vue.component('create_card', {
     template: `
-       <form>
+       <form @submit.prevent="createCard">
            <div class="form_create">
                <div class="form_div">
                     <label for="name1">Добавить задачу:</label>
-                    <input class="form_input" id="name1" v-model="name" required placeholder="task">
+                    <input class="form_input" id="name1" v-model="name" required placeholder="task" >
                 </div>
                 <div class="form_div">
                     <label for="name2">Добавить описание :</label>
-                    <textarea class="form_input" id="name2" v-model="text"></textarea>
+                    <textarea class="form_input" id="name2" v-model="text" required></textarea>
                 </div>
                 <div class="form_div">
                     <label for="name3">Добавить дэдлайн:</label>
-                    <input class="form_input" type="date" value="2023-02-02" id="name3" v-model="deadline">
+                    <input class="form_input"  type="date" value="2023-02-02" id="name3" required v-model="deadline" >
                 </div>
 
-                <input @click="createCard" class="fort_submit" type="button" value="Добавить"> 
+                <input class="fort_submit" type="submit" value="Добавить"> 
             </div>
        </form>`,
 
@@ -211,7 +260,9 @@ Vue.component('create_card', {
         return {
             name: null,
             text:null,
-            deadline: null
+            deadline: null,
+
+
 
         }
     },
@@ -221,8 +272,12 @@ Vue.component('create_card', {
                 name: this.name,
                 text: this.text,
                 data: new Date().toLocaleString(),
+                dataEdit: null,
                 deadline: this.deadline,
-                status: 0
+                status: 0,
+                edit: false,
+                cancel: false
+
 
             }
 
